523e927be1be926578c413c6f3521d65cc62cf0e
Javier <ja.becerraa@gmail.com>
2021-02-05 10:21:10 -0300

    feat: adding tailwindcss :package:

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <Nav class="mb-6" />
+    <HelloWorld msg="Welcome to Your Vue.js App" class="text-center" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import Nav from '@/components/Nav.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    Nav
   },
 };
 </script>
