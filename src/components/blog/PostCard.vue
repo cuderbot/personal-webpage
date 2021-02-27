42f2530027595d7c1e1c5073d990775017b555a0
Javier <ja.becerraa@gmail.com>
2021-02-27 00:14:32 -0300

    feat(blog): post card

---
+++ src/components/blog/PostCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="column">
+    <img />
+    <div class="tags">
+      <button
+        v-for="(tag, index) in tags"
+        :key="index"
+        class="button tag is-light"
+      >
+        {{ tag }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostCard',
+  props: {
+    tags: {
+      type: Array,
+      required: false,
+      default() {
+        return Array.of([])
+      },
+    },
+  },
+}
+</script>
