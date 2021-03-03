e47a6ddacdc38f11b0e0010310c1eadb8ddf9961
Javier <ja.becerraa@gmail.com>
2021-03-02 21:08:04 -0300

    chore: updating nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,12 @@ export default {
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: [
+    {
+      path: '~/components/layout',
+      prefix: 'layout',
+    },
+  ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
