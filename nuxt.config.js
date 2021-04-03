1aa4a324c7fc21785d369d9fe2d0e508235fb286
cuderbot <cuderboto002@gmail.com>
2021-04-03 18:08:29 -0300

    chore: moving tailwind config into separate file

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,22 +79,5 @@ export default {
     configPath: 'tailwind.config.js',
     jit: true,
     exposeConfig: false,
-    config: {
-      purge: [
-        './components/**/*.{vue,js}',
-        './layouts/**/*.vue',
-        './pages/**/*.vue',
-        './plugins/**/*.{js,ts}',
-        './nuxt.config.{js,ts}',
-      ],
-      darkMode: false, // or 'media' or 'class'
-      theme: {
-        extend: {},
-      },
-      variants: {
-        extend: {},
-      },
-      plugins: [],
-    },
   },
 }
